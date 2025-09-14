d95795f34bd02b887596a2967e19a393ce89f1d8
Michael Guzman <mgzmn4@gmail.com>
2025-09-13 21:59:59 -0500

    Added url race

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use trpl::Html;
+use trpl::{Either, Html};
 
 // takes a url string, makes a request to it and
 // returns the text of the title element
-async fn page_title(url: &str) -> Option<String> {
+async fn page_title(url: &str) -> (&str, Option<String>) {
     let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title_element| title_element.inner_html())
+        .map(|title_element| title_element.inner_html());
+    (url, title)
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!(),
+        let url1 = page_title(&args[1]);
+        let url2 = page_title(&args[2]);
+
+        let (url, maybe_title) = match trpl::race(url1, url2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("It's page title is '{title}'"),
+            None => println!("It's title could not be parsed."),
         }
     })
 }
