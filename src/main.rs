ec8645b0aac075fbef00b6c4daad9660473417a6
Michael Guzman <mgzmn4@gmail.com>
2025-09-13 22:35:32 -0500

    initial work on spawns

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use trpl::{Either, Html};
 
 // takes a url string, makes a request to it and
@@ -11,21 +13,16 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::run(async {
-        let url1 = page_title(&args[1]);
-        let url2 = page_title(&args[2]);
-
-        let (url, maybe_title) = match trpl::race(url1, url2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
-        };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("It's page title is '{title}'"),
-            None => println!("It's title could not be parsed."),
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("Hi number {i} form the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+        for i in 1..5 {
+            println!("Hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
         }
-    })
+    });
 }
