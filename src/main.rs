f2eba580e20b05b7e852e8c3dc5ce9b66aaa8ffe
Michael Guzman <mgzmn4@gmail.com>
2025-09-17 21:56:41 -0500

    Using join for futures

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,20 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
 
 fn main() {
     trpl::run(async {
-        let handle = trpl::spawn_task(async {
+        let fut1 = async {
             for i in 1..10 {
-                println!("Hi number {i} form the first task!");
+                println!("hi number {i} from the first task!");
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        });
-        for i in 1..5 {
-            println!("Hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        };
 
-        handle.await.unwrap();
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
     });
 }
